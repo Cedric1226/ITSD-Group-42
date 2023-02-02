abfe88a1397443274e6a15eea844312b2600f023
Zhang Shuo <2819601S@student.gla.ac.uk>
2023-02-02 20:26:05 +0000

    Update Initalize.java

--- app/events/Initalize.java
+++ app/events/Initalize.java
@@ -30,7 +30,7 @@ public class Initalize implements EventProcessor{
 		
 		gameState.something = true;
 
-/*
+
 		// initialize board
 		Board.initDisplayBoard(out,gameState.myBoard,0);
 
@@ -55,8 +55,8 @@ public class Initalize implements EventProcessor{
 		BasicCommands.setPlayer1Mana(out, gameState.humanPlayer);
 		BasicCommands.setPlayer2Mana(out, gameState.aiPlayer);
 
- */
-		MyDemo.executeDemo(out);
+
+		//MyDemo.executeDemo(out);
 	}
 
 }
